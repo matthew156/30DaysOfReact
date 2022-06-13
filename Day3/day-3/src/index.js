54331e338d407ca7e6dbb141c924051b5bbee3e4
Matt <matthewastro@gmail.com>
2022-06-13 14:52:17 -0400

    Completed day three with the exception of exercise 3

---
+++ Day3/day-3/src/index.js
@@ -0,0 +1,123 @@
+// index.js
+import React from 'react'
+import ReactDOM from 'react-dom'
+import csslogo from './images/css.png'
+import htmllogo from './images/html.png'
+import jslogo from './images/jslogo.png'
+import reactlogo from './images/Reactlogo.png'
+// To get the root element from the HTML document
+// JSX element, header
+const welcome = 'Welcome to 30 Days Of React'
+const title = 'Getting Started React'
+const subtitle = 'JavaScript Library'
+const author = {
+  firstName: 'Asabeneh',
+  lastName: 'Yetayeh',
+}
+const date = 'Oct 2, 2020'
+
+// JSX element, header
+const header = (
+  <header>
+    <div className='header-wrapper'>
+      <h1>{welcome}</h1>
+      <h2>{title}</h2>
+      <h3>{subtitle}</h3>
+      <p>
+        Instructor: {author.firstName} {author.lastName}
+      </p>
+      <small>Date: {date}</small>
+    </div>
+  </header>
+)
+
+const numOne = 3
+const numTwo = 2
+
+const result = (
+  <p>
+    {numOne} + {numTwo} = {numOne + numTwo}
+  </p>
+)
+
+const yearBorn = 1820
+const currentYear = new Date().getFullYear()
+const age = currentYear - yearBorn
+const personAge = (
+  <p>
+    {' '}
+    {author.firstName} {author.lastName} is {age} years old
+  </p>
+)
+
+// JSX element, main
+const techs = ['HTML', 'CSS', 'JavaScript']
+const techsFormatted = techs.map((tech) => <li>{tech}</li>)
+
+
+
+// JSX element, main
+const main = (
+  <main>
+    <div className='main-wrapper'>
+      <p>
+        Prerequisite to get started{' '}
+        <strong>
+          <em>react.js</em>
+        </strong>
+        :
+      </p>
+      <ul>{techsFormatted}</ul>
+      {result}
+      {personAge}
+    </div>
+  </main>
+)
+
+const copyRight = 'Copyright 2020'
+
+const images = (
+  <div>
+
+  </div>
+)
+
+
+// JSX element, footer
+const subscribe = (
+<div class ="subscribe">
+  <h1>Subscribe</h1>
+   <p>Sign up with your email address to receive news and updates.</p>
+   <div>
+     <input type="text" name='FirstName' placeholder='First Name'/>
+     <input type="text" name='LastName' placeholder='Last Name'/>
+     <input type="text" name='Email' placeholder='Email'/>
+   </div>
+  <button class="btn">Submit</button>
+ </div>
+
+)
+
+
+const footer = (
+  <footer>
+    <div className='footer-wrapper'>
+      <p>{copyRight}</p>
+    </div>
+  </footer>
+)
+
+// JSX element, app
+const app = (
+  <div className='app'>
+    {header}
+    {main}
+    {images}
+    {subscribe}
+    {footer}
+  </div>
+)
+
+const rootElement = document.getElementById('root')
+// we render the JSX element using the ReactDOM package
+ReactDOM.render(app, rootElement)
